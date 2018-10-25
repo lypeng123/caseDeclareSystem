75a9594fe14b6c3ac4b2201b60e47f373f64b2a0
lypeng0919 <940613931@qq.com>
2018-10-25 23:09:50 +0800

    10-25  用户案例及修改审查状态

---
+++ src/main/resources/static/js/user-case-list.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Administrator on 2017/10/9.
+ */
+$(function () {
+    //查询案例筛选
+    $.ajax({
+        type: 'post',
+        url: propertyUrl,
+        data: {},
+        xhrFields: {
+            withCredentials: true
+        },
+        crossDomain: true,
+        success: function (res) {
+            //console.log(res);
+            if (res.state == 2) {
+                $("#table_mark").hide();
+                $(".page").hide();
+                $(".table-file .data-none").show();
+                $(".table-file .data-none").html(res.message);
+
+                new Dialog().init({
+                    mask: true,
+                    title: '',
+                    content: res.message,
+                    isAlert: true,
+                    hideView:true,
+                    callback: function (boolean) {
+                        if (boolean) {
+                            top.location.href = loginUrl;
+                        }
+                    }
+                });
+
+            }
+        }
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
